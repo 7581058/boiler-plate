c9cd9e94f07ff8d77e87fb280da44d55c8599c0e
7581058 <100559990+7581058@users.noreply.github.com>
2022-03-16 16:18:39 +0900

    cors issue, proxy setting

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 function LandingPage() {
 
   useEffect(() => {
-    axios.get('https://localhost:5000/api/hello') //get리퀘스트 서버에 보냄 
+    axios.get('/api/hello') //get리퀘스트 서버에 보냄 
     .then(response => console.log(response.data)) //서버에서 돌아오는 리스폰스 콘솔에 보여주기 
   }, [])
 
